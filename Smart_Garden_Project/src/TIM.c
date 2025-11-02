541d117865196512cf43ddeae36eb7b794768062
Puscasu Dorin <puscasudorin20052@gmail.com>
2025-11-02 21:30:40 +0200

    feat(TIM): create ms counter
    - Add TIM_ms_value() function to read milliseconds
    
    - Add TIMER0_COMPA_vect ISR to increment ms_counter

--- Smart_Garden_Project/src/TIM.c
+++ Smart_Garden_Project/src/TIM.c
@@ -1,7 +1,31 @@
 #include <avr/io.h>
 #include "TIM.h"
 
+volatile uint32_t ms_counter = 0;
+
 void TIM_init(void){
-	TCCR0A = (1<<WGM01);
-	TCCR0B = (1<<CS01) | (1<<CS00); //CS->Clock Prescaler(clk/64)
+	TCCR0A = (1<<WGM01);			//Set WGM to CTC mode
+	TCCR0B = (1<<CS01) | (1<<CS00); //Set Clock Prescaler -> 16MHz / 64 = 250k / 1000ms -> 250 ticks/ms 
+	
+	OCR0A = 249;
+	
+	TIMSK0 |= (1<<OCIE0A);
+	
+	sei();
+}
+
+ISR(TIMER0_COMPA_vect){
+	ms_counter++;
+}
+
+uint32_t TIM_value(void){
+	uint32_t time;
+	
+	cli();
+	
+	time = ms_counter;
+	
+	sei();
+	
+	return time;
 }
