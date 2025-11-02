09028ba6087bea411e22644d74e1e68bf6cc67fc
Puscasu Dorin <puscasudorin20052@gmail.com>
2025-11-02 23:15:17 +0200

    chore(project): add empty system and config files

---
+++ Smart_Garden_Project/Core/inc/config.h
@@ -0,0 +1,9 @@
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+//Add global variables
+
+
+
+#endif /* CONFIG_H_ */
---
+++ Smart_Garden_Project/Core/inc/system.h
@@ -0,0 +1,16 @@
+/*
+ * system.h
+ *
+ * Created: 11/2/2025 11:10:41 PM
+ *  Author: Dorin
+ */ 
+
+
+#ifndef SYSTEM_H_
+#define SYSTEM_H_
+
+
+
+
+
+#endif /* SYSTEM_H_ */
