121e43e31e6522c7bd92025a8bf8a771b2c595c9
Puscasu Dorin <puscasudorin20052@gmail.com>
2025-11-07 20:01:34 +0200

    refactor(main): remove unnecessary comments

--- Smart_Garden_Project/Core/src/main.c
+++ Smart_Garden_Project/Core/src/main.c
@@ -17,7 +17,6 @@ int main(void)
 	
 	Peripherals_init();
 	Drivers_init();
-	char buffer[16];
 	
     while (1) 
     {
@@ -49,50 +48,5 @@ int main(void)
 		if(global_time % 7199993UL  == 0){
 			//2h for Water Level Sensor
 		}
-		
-		
-		/*
-		LCD_gotoxy(0,0);
-		//LCD_sendData(223); //Degree
-		LCD_sendData(0x5E); //UP arrow
-		LCD_sendData(0x76); //DOWN arrow
-		LCD_print("C and c");
-		*/
-		/*	
-		char strr[10];
-		sprintf(strr, "%lu", global_time);
-		UART_TransmitString(strr);
-		UART_TransmitString("\n\r");
-		
-		float number =  read_LM35_Temp();
-		char str[6];
-		
-		sprintf(str, "%.2f", number);
-		UART_TransmitString("Temp:");
-		UART_TransmitString(str);
-		UART_TransmitByte('\n');
-		UART_TransmitByte('\r');
-		LCD_gotoxy(0,0);
-		LCD_print("Temp:");
-		LCD_print(str);	
-	
-	
-		float num =  read_LightSensor_Percentages();
-		char strr[6];
-		sprintf(strr, "%.2f", num);
-		UART_TransmitString("Light:");
-		UART_TransmitString(strr);
-		UART_TransmitByte('\n');
-		UART_TransmitByte('\r');
-		//LCD_gotoxy(0,1);
-		//LCD_print("Light:");
-		//LCD_print(strr);
-		_delay_ms(1000);
-		//LCD_clear();
-		
-		//_delay_ms(1000);
-		
-		*/
-		
     }
 }
