1ecc5b21364aa33702cc026788dea6086444adae
Puscasu Dorin <puscasudorin20052@gmail.com>
2025-11-22 23:04:39 +0200

    feat(WaterLevelSensor): create initialization and read sensor functions

--- Smart_Garden_Project/Libs/inc/WaterLevelSensor.h
+++ Smart_Garden_Project/Libs/inc/WaterLevelSensor.h
@@ -1,16 +1,7 @@
-/*
- * WaterLevelSensor.h
- *
- * Created: 11/7/2025 2:24:36 PM
- *  Author: Dorin
- */ 
-
-
 #ifndef WATERLEVELSENSOR_H_
 #define WATERLEVELSENSOR_H_
 
-
-
-
-
+	float read_water_sensor(void);
+	void water_sensor_init(uint8_t channel, float V_ref_param);
+	
 #endif /* WATERLEVELSENSOR_H_ */
--- Smart_Garden_Project/Libs/src/WaterLevelSensor.c
+++ Smart_Garden_Project/Libs/src/WaterLevelSensor.c
@@ -1,6 +1,26 @@
-/*
- * WaterLevelSensor.c
- *
- * Created: 11/7/2025 2:25:46 PM
- *  Author: Dorin
- */ 
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include "WaterLevelSensor.h"
+#include "ADC.h"
+#include <util/delay.h>
+#include "config.h"
+
+static uint8_t water_sensor_channel = 0;
+static float V_ref = 5.0f;
+
+void water_sensor_init(uint8_t channel, float V_ref_param){
+	water_sensor_channel = channel;
+	V_ref = V_ref_param;
+	
+	ADC_init();
+}
+
+float read_water_sensor(void){
+	water_sensor_port |= (1 << water_sensor_pin);
+	_delay_ms(10);
+	float water_level = ADC_read_voltage(water_sensor_channel, V_ref);
+	water_sensor_port &= ~(1 << water_sensor_pin);
+	
+	return water_level;
+}
